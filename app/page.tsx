0bd79252709c5728348b4ec5dad5db663ccff890
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-01-24 22:55:45 +0530

    Fixed the button & the Form folder.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 "use client"
 
-import Form from "./form/Form";
+import Link from "next/link";
 import styles from "./home.module.css";
 
 export default function Home() {
+
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Welcome to the code challenge!</h1>
       <p className={styles.desc}>Let's start the fun of coding.Click the button below to get started.</p>
 
-      <button className={styles.button} onClick = {() => {<Form />}} >Start Challenge</button>
+      
+      <Link href= "/form" >
+        <button className={styles.button} >Start Challenge</button>
+      </Link>
     </div>
   );
 }
